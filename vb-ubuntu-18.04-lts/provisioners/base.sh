e1574092c29e8eb5468e07fb68871d27bee562ee
Marcin Morawski <marcin@morawskim.pl>
2020-04-16 18:44:28 +0200

    vb ubuntu - don't install puppet

--- vb-ubuntu-18.04-lts/provisioners/base.sh
+++ vb-ubuntu-18.04-lts/provisioners/base.sh
@@ -2,5 +2,4 @@
 set -eu
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get install -y curl git
-git clone --depth 1 https://github.com/morawskim/provision-dev-servers.git /provision
+apt-get install -y curl
