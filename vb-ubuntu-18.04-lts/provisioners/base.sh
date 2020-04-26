6d671e972858cc07eaf8eb395f4590361ae1512c
Marcin Morawski <marcin@morawskim.pl>
2020-04-26 11:04:52 +0200

    vagrant ubuntu - build box for libvirt provider

--- vb-ubuntu-18.04-lts/provisioners/base.sh
+++ vb-ubuntu-18.04-lts/provisioners/base.sh
@@ -3,3 +3,7 @@ set -eu
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get install -y curl
+
+# Disable Predictable Network Interface names and use eth0
+sed -i 's/GRUB_CMDLINE_LINUX="\(.*\)"/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0 \1"/g' /etc/default/grub;
+update-grub
